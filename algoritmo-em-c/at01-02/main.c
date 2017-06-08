5d27e80200b657fe3108f69a442ee2c8ac6be63b
AlbertoAlfredo <albertoalfredo.profissional@gmail.com>
2017-06-08 10:32:02 -0400

    Inicio dos códigos de Algoritmos 2

---
+++ algoritmo-em-c/at01-02/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    /* Declaração das variáveis */
+    char nome[30];
+
+    /*Inicio do programa*/
+    printf("Digite seu nome: "); //Saida de dados
+    scanf("$s", nome); //Leitura do teclado
+    printf("Seja bem vindo(a) a diciplina de algoritimo de Lógica de Programação II ", nome);//saida de dados
+    return(0);//retornao obrigatório
+}
