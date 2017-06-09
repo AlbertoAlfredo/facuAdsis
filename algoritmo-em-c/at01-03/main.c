7f8b2c192d209e1cf76f708ee190e01d936dff36
AlbertoAlfredo <albertoalfredo.profissional@gmail.com>
2017-06-08 22:45:20 -0400

    Inicio das dicas em C

---
+++ algoritmo-em-c/at01-03/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*Declaração das variáveis*/
+    int num, raiz, quadrado;
+
+    /*inicio da execução*/
+    printf("Digite um número inteiro positivo");//Saida de dados na tela
+    scanf("%i", num);//Entrada de dados do usuario
+
+    return 0;
+}
